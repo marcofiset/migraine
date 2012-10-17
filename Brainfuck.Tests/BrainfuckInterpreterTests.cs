c287d97544c4c170d27f38541652a68fad88e89d
Marc-Olivier Fiset <marco.fiset@gmail.com>
2012-10-17 16:06:54 -0400

    Added Brainfuck.Tests project skeleton
    This is where the unit tests for the Brainfuck assembly will go

---
+++ Brainfuck.Tests/BrainfuckInterpreterTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brainfuck.Tests
+{
+    [TestFixture]
+    public class BrainfuckInterpreterTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Brainfuck.Tests/Program.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.ConsoleRunner;
+using System;
+using System.Linq;
+
+namespace Brainfuck.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String newArgs = "";
+            args.ToList().ForEach(s => newArgs += " " + s);
+
+            Runner.Main(newArgs.Split(";".ToCharArray()));
+        }
+    }
+}
