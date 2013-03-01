595063a262edfbf15284094f92eaafb663f3aff5
Marc-Olivier Fiset <marco.fiset@gmail.com>
2013-02-28 22:29:32 -0500

    Started implementing TokenStream and tests

---
+++ Migraine.Core.Tests/TokenStreamTests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migraine.Core.Tests
+{
+    [TestFixture]
+    public class TokenStreamTests
+    {
+        private TokenStream _tokenStream;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _tokenStream = new TokenStream();
+        }
+
+        [Test]
+        public void CanAddTokenToStream()
+        {
+            _tokenStream.Add(new Token("5", TokenType.Number));
+        }
+
+        [Test]
+        public void StreamCountRaiseWhenAdd()
+        {
+            Assert.AreEqual(0, _tokenStream.Count);
+            _tokenStream.Add(new Token("5", TokenType.Number));
+            Assert.AreEqual(1, _tokenStream.Count);
+        }
+
+        [Test]
+        public void StreamCanConsumeAnyToken()
+        {
+            _tokenStream.Add(new Token("5", TokenType.Number));
+            Assert.That(_tokenStream.Consume());
+        }
+
+        [Test]
+        public void StreamCanConsumeTokenByValue()
+        {
+            _tokenStream.Add(new Token("5", TokenType.Number));
+            Assert.That(_tokenStream.Consume("5"));
+        }
+
+        [Test]
+        public void StreamCanConsumeTokenByType()
+        {
+            _tokenStream.Add(new Token("5", TokenType.Number));
+            Assert.That(_tokenStream.Consume(TokenType.Number));
+        }
+
+        [Test]
+        public void ConsumeShouldReduceCountByOne()
+        {
+            _tokenStream.Add(new Token("5", TokenType.Number));
+            _tokenStream.Consume();
+            Assert.AreEqual(0, _tokenStream.Count);
+        }
+
+        [Test]
+        public void ConsumeOnEmptyStreamThrowsException()
+        {
+            Assert.Throws(typeof(TokenStreamEmptyException), () => _tokenStream.Consume());
+        }
+    }
+}
---
+++ Migraine.Core/TokenStream.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migraine.Core
+{
+    public class TokenStreamEmptyException : Exception
+    {
+        private static String _message = "Can't consume token on an empty stream.";
+
+        public TokenStreamEmptyException() : base(_message) { }
+    }
+
+    public class TokenStream
+    {
+        private Queue<Token> _queue;
+
+        public TokenStream()
+        {
+            _queue = new Queue<Token>();
+        }
+
+        public void Add(Token token)
+        {
+            _queue.Enqueue(token);
+        }
+
+        public Boolean Consume()
+        {
+            if (_queue.Count == 0)
+                throw new TokenStreamEmptyException();
+
+            _queue.Dequeue();
+            return true;
+        }
+
+        public Boolean Consume(string tokenValue)
+        {
+            return true;
+        }
+
+        public Boolean Consume(TokenType tokenType)
+        {
+            return true;
+        }
+
+        public Int32 Count 
+        {
+            get { return _queue.Count; }
+        }
+    }
+}
