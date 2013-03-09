650c4585c3de39c2be1db8b8ee8f65264006b281
Marc-Olivier Fiset <marco.fiset@gmail.com>
2013-03-08 21:33:32 -0500

    Unit test MigraineLexer

---
+++ Migraine.Core.Tests/MigraineLexerTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migraine.Core.Tests
+{
+    [TestFixture]
+    public class MigraineLexerTests
+    {
+        private MigraineLexer _lexer;
+
+        private void Verify(String program, TokenStream tokens)
+        {
+            program.Split(' ').ToList().ForEach(s => Assert.IsTrue(tokens.Consume(s)));
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _lexer = new MigraineLexer();
+        }
+
+        [Test]
+        public void CanProcudeNumberTokens()
+        {
+            var program = "5 6.5 0.523";
+            var tokens = _lexer.Tokenize(program);
+
+            Verify(program, tokens);
+        }
+
+        [Test]
+        public void CanProduceIdentifierTokens()
+        {
+            var program = "var1 var_2 VAR3 Var_4 _898";
+            var tokens = _lexer.Tokenize(program);
+
+            Verify(program, tokens);
+        }
+
+        [Test]
+        public void CanProduceOperatorTokens()
+        {
+            var program = "+ - * / ( )";
+            var tokens = _lexer.Tokenize(program);
+
+            Verify(program, tokens);
+        }
+    }
+}
