17cbc937668ac2a51eca5773bfaf38408a7a6fcb
Marc-Olivier Fiset <marco.fiset@gmail.com>
2013-04-25 11:43:47 -0400

    Added basic scope class and tests

---
+++ Migraine.Core.Tests/ScopeTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Migraine.Core.Tests
+{
+    public class ScopeTests
+    {
+        private Scope _parentScope;
+        private Scope _innerScope;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _parentScope = new Scope();
+            _parentScope.AssignVariable("var1", 5);
+            _parentScope.AssignVariable("var2", 6);
+
+            _innerScope = new Scope(_parentScope);
+            _innerScope.AssignVariable("innerVar", 12);
+        }
+
+        [Test]
+        public void ScopeCanResolveVariableName()
+        {
+            Assert.AreEqual(5, _parentScope.ResolveVariable("var1"));
+        }
+
+        [Test]
+        public void ResolveThrowsOnUnknownVariable()
+        {
+            Assert.Throws<UndefinedIdentifierException>(() => _parentScope.ResolveVariable("unknown"));
+        }
+
+        [Test]
+        public void CanResolveVariablesInParentScopes()
+        {
+            Assert.AreEqual(5, _innerScope.ResolveVariable("var1"));
+        }
+    }
+}
---
+++ Migraine.Core/Scope.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migraine.Core
+{
+    public class UndefinedIdentifierException : Exception
+    {
+        public UndefinedIdentifierException(String identifier) : base("Identifier undefined : " + identifier) { }
+    }
+
+    public class Scope
+    {
+        private Scope _parent;
+        private Dictionary<String, Double> _variables;
+
+        /// <summary>
+        /// Creates a new scope instance
+        /// </summary>
+        /// <param name="parent">The parent scope that the new one is tied to</param>
+        public Scope(Scope parent = null)
+        {
+            _parent = parent;
+            _variables = new Dictionary<String, Double>();
+        }
+
+        /// <summary>
+        /// Defines a variable in the scope
+        /// </summary>
+        /// <param name="name">The name of the variable</param>
+        /// <param name="value">The value of the variable</param>
+        public void AssignVariable(String name, Double value)
+        {
+            _variables.Add(name, value);
+        }
+
+        /// <summary>
+        /// Resolves a variable in the current scope.
+        /// If the variable does not exist in the current scope, we walk up
+        /// the parent scopes until we find it or utimately throw an exception
+        /// if no variable exists with the name given
+        /// </summary>
+        /// <param name="name">The name of the variable</param>
+        /// <returns>The value of the variable</returns>
+        public Double ResolveVariable(String name)
+        {
+            if (_variables.ContainsKey(name))
+                return _variables[name];
+
+            if (_parent == null)
+                throw new UndefinedIdentifierException(name);
+
+            return _parent.ResolveVariable(name);
+        }
+    }
+}
